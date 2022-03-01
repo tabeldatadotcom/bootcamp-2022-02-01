bd87576b978474d1f68ffc9b9bdca5836c2af1ab
dimasm93 <software.dimas_m@icloud.com>
2022-03-01 11:13:14 +0700

    update set data from request

--- src/main/java/com/tabeldata/bootcamp/web/controller/HaloController.java
+++ src/main/java/com/tabeldata/bootcamp/web/controller/HaloController.java
@@ -1,9 +1,7 @@
 package com.tabeldata.bootcamp.web.controller;
 
 import com.tabeldata.bootcamp.web.model.Example;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -14,12 +12,23 @@ import java.time.LocalDateTime;
 public class HaloController {
 
     @GetMapping(value = "/show")
-    public Example showData(){
+    public Example showData(
+            @RequestParam(name = "msg", required = true) String message,
+            @RequestParam(name = "oth", required = false) String other,
+            BigDecimal salary,
+            Boolean active){
         Example data = new Example();
-        data.setMessage("halo saya sedang belajar web service dengan json");
-        data.setOther("test");
-        data.setSalary(new BigDecimal(10000));
-        data.setIsActive(false);
+        data.setMessage(message);
+        data.setOther(other);
+        data.setSalary(salary);
+        data.setIsActive(active);
+        data.setTransactionDate(LocalDate.now());
+        data.setTransactionDatetime(LocalDateTime.now());
+        return data;
+    }
+
+    @GetMapping(value = "/input")
+    public Example inputData(@RequestBody Example data){
         data.setTransactionDatetime(LocalDateTime.now());
         data.setTransactionDate(LocalDate.now());
         return data;
